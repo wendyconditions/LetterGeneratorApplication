d58d5015941836951fe5f49cd99dc75850dae9d5
Wendy Conditions <wam922@gmail.com>
2017-10-24 16:39:18 -0700

    Wip

--- Content/js/controller.js
+++ Content/js/controller.js
@@ -11,6 +11,7 @@
         var vm = this;
         vm.data = {};
         vm.btnUrl = _btnUrl;
+
         ///////////////
 
         function _btnUrl(url) {
@@ -36,47 +37,40 @@
                 obj.quals.push(qualifications);
             }
 
-
             for (var i = 0; i < obj.quals.length; i++) {
                 var sass = "I am good at Sass";
-                var html = "10+ years of HTML5 and CSS3 experience";
+                var html = "Experience developing in HTML, CSS, and JavaScript, specifically jQuery libraries";
                 var angular = "Equivalent of 2 years professional experience in AngularJS";
                 var sql = "Equivalent of 2 years professional T-SQL experience using Microsoft SQL Server/ Studio Mangement";
                 var css = "My css is excellent";
+                var js = "Fluent in JavaScript, ES5, ES6";
+                var slack = "Created custom slack bots";
+                var git = "Used GitHub and Git for version control on most projects";
+                var jquery = "2+ years experience with javascript libraries";
+
                 if (String(obj.quals[i].qual).match(/SASS/, 'g')) {
                     obj.quals[i].match = sass;
                 } else if (String(obj.quals[i].qual).toLowerCase().match(/html/, 'g')) {
                     obj.quals[i].match = html;
                 } else if (String(obj.quals[i].qual).toLowerCase().match(/angular/, 'g')) {
                     obj.quals[i].match = angular;
-                } else if (String(obj.quals[i].qual).toLowerCase().match(/CSS/, 'g')) {
+                } else if (String(obj.quals[i].qual).toLowerCase().match(/css/, 'g')) {
                     obj.quals[i].match = css;
                 } else if (String(obj.quals[i].qual).toLowerCase().match(/sql/, 'g')) {
                     obj.quals[i].match = sql;
+                } else if (String(obj.quals[i].qual).toLowerCase().match(/javascript/, 'g')) {
+                    obj.quals[i].match = js;
+                } else if (String(obj.quals[i].qual).toLowerCase().match(/slack/, 'g')) {
+                    obj.quals[i].match = slack;
+                } else if (String(obj.quals[i].qual).toLowerCase().match(/git/, 'g')) {
+                    obj.quals[i].match = git;
                 } else {
                     obj.quals[i].match = null;
                 }
             }
-
             vm.jobInfo.push(obj);
             console.log(vm.jobInfo);
         }
-
-        // I might need this for later, WIP
-
-        //for (var i = 0; i < response.data.job.length; i++) {
-        //    var specs = response.data.job[i].quals;
-        //    for (var k = 0; k < specs.length; k++) {
-        //        if (String(specs[k]).match(/HTML/, 'g')) {
-        //            qualifications.push(specs[k]);
-        //        } else if (String(specs[k]).match(/responsive design/, 'g')) {
-        //            qualifications.push(specs[k]);
-        //        } 
-        //    }
-        //    console.log(qualifications);
-        //}
-
-
 
         function _btnUrlError() {
             // Need to handle this error properly, alert service 
